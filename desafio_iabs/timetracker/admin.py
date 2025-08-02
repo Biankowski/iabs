73c8682d897e41dbfa05a432730bf1e3fb2f50c3
Rodrigo Torres <rodrigobianchini96@gmail.com>
2025-08-02 19:10:44 -0300

    chore: register admin

--- desafio_iabs/timetracker/admin.py
+++ desafio_iabs/timetracker/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Task, TimeEntry
 
-# Register your models here.
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('user', 'description', 'created_at')
+    search_fields = ('user__username', 'description')
+    list_filter = ('created_at',)
+
+@admin.register(TimeEntry)
+class TimeEntryAdmin(admin.ModelAdmin):
+    list_display = ('task', 'entry_date', 'duration', 'description', 'created_at')
+    search_fields = ('task__description', 'description')
+    list_filter = ('entry_date', 'created_at')
