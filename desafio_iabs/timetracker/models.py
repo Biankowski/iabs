b8cc599d4e28b1fc3bcbf32552ff74dc56a57cbb
Rodrigo Torres <rodrigobianchini96@gmail.com>
2025-08-02 18:41:51 -0300

    chore: created data models

--- desafio_iabs/timetracker/models.py
+++ desafio_iabs/timetracker/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.contrib.auth.models import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class Task(models.Model):
+    user = models.ForeignKey(
+        User, 
+        on_delete=models.CASCADE,
+        related_name='tasks',
+        verbose_name='Usuário'
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Criado em'
+    )
+
+    description = models.TextField(
+        verbose_name='Descrição'
+    )
+
+class TimeEntry(models.Model):
+    task = models.ForeignKey(
+        Task,
+        on_delete=models.CASCADE,
+        related_name='time_entries',
+        verbose_name='Tarefa'
+    )
+
+    entry_date = models.DateField(
+        verbose_name='Data de entrada'
+    )
+
+    duration = models.DurationField(
+        verbose_name='Duração'
+    )
+
+    description = models.TextField(
+        blank=True,
+        verbose_name='Descrição'
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Criado em'
+    )
+    
