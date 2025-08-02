7e633d673fc067110afbab68a5725031360735e9
Rodrigo Torres <rodrigobianchini96@gmail.com>
2025-08-02 20:51:31 -0300

    chore: created list_tasks views

--- desafio_iabs/timetracker/views.py
+++ desafio_iabs/timetracker/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Task, TimeEntry
 
-# Create your views here.
+def list_tasks(request):
+    tasks = Task.objects.select_related('user').all()
+    
+    return render(request, 'timetracker/task_list.html', {'tasks': tasks})
